ea94aa95d70afa49b7e14af0b8c72753bdd87f77
kkumyk <k.kumykova@gmail.com>
2024-02-28 15:51:08 +0000

    Add explanation notes for the Length of the longest substring solution

--- length-of-the-longest-substring.py
+++ length-of-the-longest-substring.py
@@ -1,15 +1,26 @@
+"""
+charSet - a box where we keep track of the letters we have seen SO FAR without repeating
+
+l and r:
+    - left and right pointers will determine a sliding window 
+    - point to different positions in the string
+    - help to define a substring between them
+    - l (left pointer) will be initialised with 0
+    - r (right pointer) will be changing as we visit every character while it will be using in the for loop
+    - r (right) will go over every single character
+    
+"""
+
 class Solution:
     def lengthOfTheLongestSubstring(self, s: str) -> int:
         
         charSet = set() 
-        # left and right pointers will determine a sliding window 
-        l = 0 # our left will be initialised with 0
+        l = 0
         res = 0
-
-        # the right one will be changing as we visit every character while it will be using in the for loop
         
-        for r in range(len(s)): # the right pointer will go over every single character
-            while s[r] in charSet: # if we come across a duplicate char with the right pointer r, we will update our set
+        for r in range(len(s)):
+            #  if s[r] not in seen, we can keep increasing the window size by moving right pointer
+            while s[r] in charSet: # if we come across a duplicate char with r, we will update our set
                 charSet.remove(s[l]) # remove that duplicate until that duplicate remains in char set
                 l += 1 
             charSet.add(s[r])
